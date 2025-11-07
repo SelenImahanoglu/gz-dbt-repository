85406351ced64d3ed759bfe39c586ad5b3387177
Selen İmahanoğlu <imahanogluselen@gmail.com>
2025-11-07 16:35:55 +0000

    null değerleri değiştirildi

--- models/mart/finance/finance_campaigns_day.sql
+++ models/mart/finance/finance_campaigns_day.sql
@@ -1,18 +1,17 @@
 SELECT
-    f.date_date AS date,
-    (f.operational_margin - c.total_spend) AS ads_margin,
-    f.average_basket,
-    f.operational_margin,
-    c.total_spend AS ads_cost,
-    c.total_impressions AS ads_impression,
-    c.total_clicks AS ads_clicks,
-    f.quantity,
-    f.revenue,
-    f.purchase_cost,
-    f.margin,
-    f.shipping_fee,
-    f.log_cost,
-    f.ship_cost
+    f.date AS date,
+    (f.total_operational_margin - IFNULL(c.total_spend, 0)) AS ads_margin,
+    f.avg_cart AS average_basket,
+    f.total_operational_margin AS operational_margin,
+    IFNULL(c.total_spend, 0) AS ads_cost,
+    IFNULL(c.total_impressions, 0) AS ads_impression,
+    IFNULL(c.total_clicks, 0) AS ads_clicks,
+    f.total_quantity AS quantity,
+    f.total_revenue AS revenue,
+    f.total_purchase_cost AS purchase_cost,
+    f.total_operational_margin AS margin,
+    f.total_shipping_fees AS shipping_fee,
+    f.total_logistic_costs AS log_cost
 FROM {{ ref('finance_days') }} AS f
 LEFT JOIN {{ ref('int_campaigns_day') }} AS c
-  ON f.date_date = c.date_date
+  ON f.date = c.date_date
