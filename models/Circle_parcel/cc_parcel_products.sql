ab3976bd46a9006d32bf7dcf0cdf65f97c23bdcc
Selen İmahanoğlu <imahanogluselen@gmail.com>
2025-11-14 12:10:10 +0000

    cc_parcel_products final modelinde hem ürün bazlı kolonlar hem parcel KPI bilgileri olsun diye join yapıldı

--- models/Circle_parcel/cc_parcel_products.sql
+++ models/Circle_parcel/cc_parcel_products.sql
@@ -1,7 +1,29 @@
-{{ config(materialized='table') }}
+{{ config(
+    materialized = 'table',
+    partition_by = {
+      "field": "date_purchase",
+      "data_type": "date"
+    }
+) }}
 
 SELECT
-    parcel_id,
-    model_name,
-    quantity
-FROM {{ ref('stg_cc_parcel_products') }}
+    p.parcel_id,
+    p.model_name,
+    p.quantity,
+
+    c.date_purchase,
+    c.date_shipping,
+    c.date_delivery,
+    c.status,
+    c.qty,
+    c.nb_products,
+    c.delivery_time,
+    c.expedition_time,
+    c.transport_time,
+    c.delay,
+    c.priority,
+    c.transporter
+
+FROM {{ ref('stg_cc_parcel_products') }} p
+LEFT JOIN {{ ref('cc_parcel') }} c
+USING (parcel_id)
